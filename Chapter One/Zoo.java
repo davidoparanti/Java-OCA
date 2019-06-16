24ddc995af7591e211896f1657ebffe2fbb58b87
David Oparanti <david.oparanti@gmail.com>
2019-06-17 00:42:34 +0100

    Zoo.java modified

--- Chapter One/Zoo.java
+++ Chapter One/Zoo.java
@@ -1,6 +1,7 @@
 public class Zoo {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
 
     }
 }
