ad93c182dfac1054421c92abb61cece0cde1baa9
David Oparanti <david.oparanti@gmail.com>
2019-06-17 01:18:12 +0100

    Conflict.java added

---
+++ Chapter One/Conflict.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+import java.sql.*; 
+public class Conflict {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println("There is a conflict");
+    }
+}
